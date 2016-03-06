57cb63875f74de3596ae498bdb61f161786323aa
megos <gmegos.27@gmail.com>
2016-03-06 18:37:18 +0900

    jsが読み込まれていなかった問題を修正

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('port', (process.env.PORT) || 3000);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/static'));
 
 app.post('/api', function(req, res) {
 
